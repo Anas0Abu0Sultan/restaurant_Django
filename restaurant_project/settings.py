6eb149ad8f521902a7da47ecf37af055d1264b31
Anas0Abu0Sultan <anas.b.sn300@gmail.com>
2025-03-26 10:50:51 +0200

    donne

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -26,7 +26,13 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1:8000','Star-Restaurant.com','www.Star-Restaurant.com']
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1:8000',
+                 'Star-Restaurant.com',
+                 'www.Star-Restaurant.com',
+                 'star-restaurant.onrender.com',
+                 'www.star-restaurant.onrender.com'
+                 ]
 
 
 # Application definition
